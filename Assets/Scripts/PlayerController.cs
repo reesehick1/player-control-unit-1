ff6861809ed06f3d8c172c4247078cae82244ab5
Hickman_Reese <hickr587@hsd.k12.or.us>
2025-02-28 10:46:22 -0800

    turning and driving

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -9,9 +13,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
     }
 
     // Update is called once per frame
+   
     void Update()
     {
-      // Move the vehicle forward  
-      transform.Translate(Vector3.forward * Time.deltaTime * 20);
+      // Move the vehicle forward
+      horizontalInput = Input.GetAxis("Horizontal");
+      forwardInput = Input.GetAxis("Vertical");
+    //moves bus forward
+      transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+    //rotates the car
+      transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
