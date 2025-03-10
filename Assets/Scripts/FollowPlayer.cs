7612998c02b9ee38d7c11390f237d417d1552942
Hickman_Reese <hickr587@hsd.k12.or.us>
2025-03-10 11:16:46 -0700

    fixing dumb thing

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -10,7 +10,7 @@ public class FollowPlayer : MonoBehaviour
 
     // Update is called once per frame
     public GameObject player;
-    private Vector3 offset = new Vector3(0,5,-7);
+    private Vector3 offset = new Vector3(0,8,-10);
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
